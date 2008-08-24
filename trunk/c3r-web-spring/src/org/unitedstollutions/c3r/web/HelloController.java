b6f6eaca5a19daf85adeff0d4bca1751b04a0be1
rubenstoll@hotmail.com <rubenstoll@hotmail.com@2432d3a0-2352-0410-887a-192f08656532>
2008-08-24 21:54:09 +0000

    initial upload

---
+++ trunk/c3r-web-spring/src/org/unitedstollutions/c3r/web/HelloController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.unitedstollutions.c3r.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * @author ruben.stoll
+ * 
+ */
+public class HelloController implements Controller {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		logger.info("Returning hello view");
+		return new ModelAndView("jsp/hello.jsp");
+	}
+}
---
+++ trunk/c3r-web-spring/war/jsp/include.jsp
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<%@ page session="false"%>
+<%@ taglib uri="http://ns.inria.fr/sewese/semtags/" prefix="stl" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
